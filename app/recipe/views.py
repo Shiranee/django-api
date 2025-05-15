4593a82eb34737c41b71e3f881b6807e89417974
Shiranai <viniciuspazo@yahoo.com.br>
2025-05-15 20:18:30 -0300

    something

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -25,4 +25,7 @@ class RecipeViewSet(viewsets.ModelViewSet):
             return serializers.RecipeSerializer
 
         return self.serializer_class
-        
+
+    def perform_create(self, serializer): # validates the inputed fields to acept only the fields of the serializer
+        """Create a new recipe."""
+        serializer.save(user=self.request.user)
